2761a46250cb9a2efa62a158376ca0c13d500521
Andrew Hansbrough <hansbroughku@gmail.com>
2020-06-07 17:57:10 -0500

    Added Port number to connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
+  port: 3306,
   user: "root",
   password: "password",
   database: "employees_db"
@@ -10,8 +11,6 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-// Setting up connection.query to use promises instead of callbacks
-// This allows us to use the async/await syntax
 connection.query = util.promisify(connection.query);
 
 module.exports = connection;
