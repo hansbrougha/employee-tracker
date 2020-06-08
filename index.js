0af5dae2c1e1c35d5b1a910de7804788624227d3
Andrew Hansbrough <hansbroughku@gmail.com>
2020-06-07 19:14:56 -0500

    Completed Switch case for all options, added async function for employeesByDepartment

--- index.js
+++ index.js
@@ -73,10 +73,30 @@ async function choicePrompts() {
   switch (choice) {
     case "GET_EMPLOYEES":
       return getEmployees();
-    case "GET_ROLES":
-      return getRoles();
+    case "EMPLOYEES_BY_DEPARTMENT":
+      return employeesByDepartment();
+    case "EMPLOYEES_BY_MANAGER":
+      return employeesByManager();
+    case "ADD_EMPLOYEE":
+      return addEmployee();
+    case "DELETE_EMPLOYEE":
+      return removeEmployee();
+    case "UPDATE_ROLE":
+      return updateRole();
+    case "UPDATE_MANAGER":
+      return updateManager();
     case "GET_DEPARTMENTS":
       return getDepartments();
+    case "ADD_DEPARTMENT":
+      return addDepartment();
+    case "DELETE_DEPARTMENT":
+      return deleteDepartment();
+    case "GET_ROLES":
+      return getRoles();
+    case "ADD_ROLE":
+      return addRole();
+    case "DELETE_ROLE":
+      return deleteRole();
     default:
       return quit();
   }
@@ -84,6 +104,24 @@ async function choicePrompts() {
 //View Employees
 async function getEmployees() {
   const employees = await db.allEmployees();
+  console.table(employees);
+  choicePrompts();
+}
+
+async employeesByDepartment() {
+  const departments = await db.allDepartments();
+  const departmentChoices = departments.map(({ id, name }) => ({
+    name: name,
+    value: id
+  }));
+  const { departmentId } = await prompt([
+    {
+      type: "list",
+      name: "departmentId",
+      message: departmentChoices
+    }
+  ]);
+  const employees = await db.employeesByDepartment(departmentId);
   console.table(employees);
   choicePrompts();
 }
